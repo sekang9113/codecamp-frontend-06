8c72d5f7948c02ac6bcd734b9f67c5fe6a1cff46
sekang9113 <sungeun@peaceui-MacBookPro.local>
2022-03-14 18:41:51 +0900

    잔디

---
+++ class/pages/01-01-qqq/aaa/bbb/index.js
@@ -0,0 +1,11 @@
+export default function AAAPage() {
+
+    // 여기는 자바스크립트 쓰는곳
+
+  return (
+    <div>
+        안녕하세요!!!
+        <input type="text"/>
+    </div>
+  )
+}
---
+++ class/pages/01-02-emotion/index.js
@@ -0,0 +1,17 @@
+import {MyTitle, MyEmail} from '../../styles/emotion'
+
+export default function AAAPage() {
+
+    // 여기는 자바스크립트 쓰는곳
+
+  return (
+    <MyTitle>
+        안녕하세요!!!
+        <MyEmail type="text"/>
+        <img src="/aaa/lotto.png"/>
+    </MyTitle>
+  )
+}
+
+
+
